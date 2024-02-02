266c38a34cf3a9c8cd3c72506793eacc31d7c8a6
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2024-02-02 22:31:19 +0800

    Remove outdated education information and add professional experience

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -24,22 +24,35 @@
 
                     <h3 class="resume-title">Education</h3>
                     <div class="resume-item">
-                        <h4>Information &amp; Communication Technology</h4>
-                        <h5>2016-2018</h5>
-                        <p><em>AMA Computer College Lucena</em></p>
-                        <p> I graduated at AMA Computer School at the age of 18 and helped me a lot at computer skills and
-                            programming.</p>
-                    </div>
-                    <div class="resume-item">
                         <h4>Information Technology</h4>
                         <h5>2018 - Present</h5>
                         <p><em>Dalubhasaan ng Lungsod ng Lucena</em></p>
                         <p> I'm currently studying at this school. They helps us to be a better computer IT and programmer.
                         </p>
                     </div>
+
+                    <div class="resume-item">
+                        <h4>Information &amp; Communication Technology</h4>
+                        <h5>2016-2018</h5>
+                        <p><em>AMA Computer College Lucena</em></p>
+                        <p> I graduated at AMA Computer School at the age of 18 and helped me a lot at computer skills and
+                            programming.</p>
+                    </div>
+
                 </div>
                 <div class="col-lg-6">
                     <h3 class="resume-title">Professional Experience</h3>
+                    <div class="resume-item">
+                        <h4>Software Developer - LGU Lucena</h4>
+                        <h5>2022 - Present</h5>
+                        <p><em>Lucena City, Quezon Philippines</em></p>
+                        <ul>
+                            <li>Implemented the Business Permit Licensing System</li>
+                        </ul>
+                        <ul>
+                            <li>Contributed to the implementation of a Human Resource Information System</li>
+                        </ul>
+                    </div>
                     <div class="resume-item">
                         <h4>Web Maker Employee</h4>
                         <h5>2018 - 2019</h5>
@@ -54,5 +67,6 @@
                 </div>
             </div>
         </div>
-</section>
-<!-- End Resume Section --></template>
+    </section>
+    <!-- End Resume Section -->
+</template>
