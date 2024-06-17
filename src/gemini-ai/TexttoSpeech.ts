292fb19f2b193db488a743339b5ba9b572577aaa
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2024-06-17 16:33:56 +0800

    feat: Update TTS speak method to stop before speaking
    This commit updates the speak method in the TexttoSpeech.ts file to check if the speech synthesis is active before speaking. If it is active, the method stops the current speech before starting a new one. This ensures that the speech is not interrupted or overlapped, providing a better user experience.

--- src/gemini-ai/TexttoSpeech.ts
+++ src/gemini-ai/TexttoSpeech.ts
@@ -1,14 +1,14 @@
-import { useSpeechSynthesis } from '@vueuse/core'
+import { UseSpeechSynthesisOptions, useSpeechSynthesis } from '@vueuse/core'
 export class TTS {
     message: string;
     constructor({ message }: { message: string }) {
         this.message = message;
     }
-
     async speak() {
         try {
-            this.stop();
+            //check if the speak is active
             const tts = useSpeechSynthesis(this.message, { volume: 1, rate: 1.3, pitch: 1, lang: 'en-US', })
+            if(tts.isPlaying) tts.stop()
             tts.speak()
         } catch (e) {
             console.log(e)
