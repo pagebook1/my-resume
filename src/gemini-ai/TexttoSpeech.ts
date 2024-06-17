ac77440b68d253fd162bdef6543a153bf3b5df51
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2024-06-17 16:25:38 +0800

    feat: Update TTS rate to 1.3 for improved speech speed
    This commit updates the TTS rate in the TexttoSpeech.ts file to 1.3. By reducing the rate from 1.6 to 1.3, the speech speed is adjusted to provide a better user experience. This change ensures that the text is spoken at a more comfortable pace for the user.

--- src/gemini-ai/TexttoSpeech.ts
+++ src/gemini-ai/TexttoSpeech.ts
@@ -8,7 +8,7 @@ export class TTS {
     async speak() {
         try {
             this.stop();
-            const tts = useSpeechSynthesis(this.message, { volume: 1, rate: 1.6, pitch: 1, lang: 'en-US', })
+            const tts = useSpeechSynthesis(this.message, { volume: 1, rate: 1.3, pitch: 1, lang: 'en-US', })
             tts.speak()
         } catch (e) {
             console.log(e)
